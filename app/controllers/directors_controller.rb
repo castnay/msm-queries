4b409aba50ddba26f1aba584eb17f0a2a27d9e54
nayla_techprep <114969597+castnay@users.noreply.github.com>
2023-04-26 19:45:30 +0000

    began adding youngest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -19,6 +19,13 @@ class DirectorsController < ApplicationController
     the_id = params.fetch("an_id")
 
     @the_director = Director.where({ :id => the_id }).at(0)
+    @filmography = Movie.where({ :director_id => @the_director.id })
+
     render({ :template => "director_templates/show.html.erb" })
+  end
+
+  def green
+
+    render({ :template => "director_templates/junior.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "list_of_directors" })
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+  get("/directors/youngest", { :controller => "directors", :action => "green"})
   get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+  
 end
