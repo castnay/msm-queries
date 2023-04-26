bcf841b1df54d309d2d85974a83aa371b9e0fde9
nayla_techprep <114969597+castnay@users.noreply.github.com>
2023-04-26 16:28:59 +0000

    created first director details page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,8 +6,19 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index.html.erb" })
   end
 
-  def wisest
+  def wisest 
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
 
     render({ :template => "director_templates/eldest.html.erb" })
+  end
+
+  def director_details
+    
+    #Parameters: {"an_id"=>"42"}
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).at(0)
+    render({ :template => "director_templates/show.html.erb" })
   end
 end
